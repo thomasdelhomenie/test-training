fa4fc7fe51e7cb0ba31e344e2ab2f6daf019efe0
thomasdelhomenie <thomas.delhomenie@gmail.com>
2012-09-21 14:36:10 +0200

    First commit

---
+++ slippy-exo-common/slippy-all.js
@@ -0,0 +1,25 @@
+// Slippy core file and dependencies
+document.write('<scr'+'ipt type="text/javascript" src="../../slippy/src/jquery.min.js" ></scr'+'ipt>');
+document.write('<scr'+'ipt type="text/javascript" src="../../slippy/src/jquery.history.js" ></scr'+'ipt>');
+document.write('<scr'+'ipt type="text/javascript" src="../../slippy/src/slippy.js" ></scr'+'ipt>');
+document.write('<scr'+'ipt type="text/javascript" src="../../slippy-exo-common/slippy-init.js" ></scr'+'ipt>');
+// Syntax highlighting core file
+document.write('<scr'+'ipt type="text/javascript" src="../../slippy/src/highlighter/shCore.js" ></scr'+'ipt>');
+// Syntax highlighting brushes, remove those you don't need
+document.write('<scr'+'ipt type="text/javascript" src="../../slippy/src/highlighter/shBrushBash.js" ></scr'+'ipt>');
+//document.write('<scr'+'ipt type="text/javascript" src="../../slippy/src/highlighter/shBrushCpp.js" ></scr'+'ipt>');
+//document.write('<scr'+'ipt type="text/javascript" src="../../slippy/src/highlighter/shBrushCSharp.js" ></scr'+'ipt>');
+document.write('<scr'+'ipt type="text/javascript" src="../../slippy/src/highlighter/shBrushCss.js" ></scr'+'ipt>');
+//document.write('<scr'+'ipt type="text/javascript" src="../../slippy/src/highlighter/shBrushDelphi.js" ></scr'+'ipt>');
+document.write('<scr'+'ipt type="text/javascript" src="../../slippy/src/highlighter/shBrushDiff.js" ></scr'+'ipt>');
+document.write('<scr'+'ipt type="text/javascript" src="../../slippy/src/highlighter/shBrushGroovy.js" ></scr'+'ipt>');
+document.write('<scr'+'ipt type="text/javascript" src="../../slippy/src/highlighter/shBrushJava.js" ></scr'+'ipt>');
+document.write('<scr'+'ipt type="text/javascript" src="../../slippy/src/highlighter/shBrushJScript.js" ></scr'+'ipt>');
+//document.write('<scr'+'ipt type="text/javascript" src="../../slippy/src/highlighter/shBrushPhp.js" ></scr'+'ipt>');
+document.write('<scr'+'ipt type="text/javascript" src="../../slippy/src/highlighter/shBrushPlain.js" ></scr'+'ipt>');
+//document.write('<scr'+'ipt type="text/javascript" src="../../slippy/src/highlighter/shBrushPython.js" ></scr'+'ipt>');
+//document.write('<scr'+'ipt type="text/javascript" src="../../slippy/src/highlighter/shBrushRuby.js" ></scr'+'ipt>');
+//document.write('<scr'+'ipt type="text/javascript" src="../../slippy/src/highlighter/shBrushScala.js" ></scr'+'ipt>');
+document.write('<scr'+'ipt type="text/javascript" src="../../slippy/src/highlighter/shBrushSql.js" ></scr'+'ipt>');
+//document.write('<scr'+'ipt type="text/javascript" src="../../slippy/src/highlighter/shBrushVb.js" ></scr'+'ipt>');
+document.write('<scr'+'ipt type="text/javascript" src="../../slippy/src/highlighter/shBrushXml.js" ></scr'+'ipt>');
